c4fa4e5af7bc244ddc684b00ac9dc2473083e2d3
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-11-23 18:54:54 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "eu-west-3"
+  shared_credentials_file = "${var.shared_credentials_file}"
+}
+
+resource "aws_lightsail_key_pair" "deployer" {
+  name   = "deployer-key"
+  public_key = "${var.ssh_key}"
+
+}
+
+resource "aws_lightsail_instance" "email" {
+  name = "email_server"
+  availability_zone = "eu-west-3c"
+  key_pair_name = "deployer-key"
+  blueprint_id= "ubuntu_18_04"
+  bundle_id= "small_2_0"
+}
+
+output "ip" {
+  value = aws_lightsail_instance.email.public_ip_address
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "shared_credentials_file" {
+  type = string
+  description = "Path for the file containing aws credentials"
+}
+
+variable "ssh_key" {
+  type = string
+  description = "SSH Key to access the instance"
+}
